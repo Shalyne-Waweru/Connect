7cd120065e71e4cf65932412c88145fa5f0bb474
Shalyne-Waweru <shalynewaweru@gmail.com>
2022-06-04 23:28:06 +0300

    Add and connect the templates and the static files

--- connect/views.py
+++ connect/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+  '''
+  View function that renders the landing page and its data
+  '''
+  return render(request, 'index.html')
