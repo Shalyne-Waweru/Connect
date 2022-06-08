73f281fdbee81cf5f8ac371474a78d49199d1af1
Shalyne-Waweru <shalynewaweru@gmail.com>
2022-06-08 11:08:41 +0300

    Modify the email configurations

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -19,7 +19,7 @@ import cloudinary.uploader
 import cloudinary.api
 
 # Email configurations
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
