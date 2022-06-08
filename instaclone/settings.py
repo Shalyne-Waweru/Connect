41d86611005829aceef4d7edaf75ebe8e240987f
Shalyne-Waweru <shalynewaweru@gmail.com>
2022-06-08 11:26:25 +0300

    Modify the STATICFILES_STORAGE settings

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -147,7 +147,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
 MEDIAFILES_DIRS = (
